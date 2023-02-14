c2b5cc82232a85f50e5a6a8c45215b0ecd1b0743
Alexey <idreamzzer@gmail.com>
2023-02-14 17:35:27 +0300

    made basic pixel button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,34 @@
 import styles from "./Button.module.css";
 import classNames from "classnames";
+import { lightenDarkenColor } from "../../utils";
 
-export interface IButtonProps {}
+export interface IButtonProps {
+	backgroundColor?: string;
+	fontColor?: string;
+	fontBold?: boolean;
+}
 
-export function Button(props: IButtonProps) {
-	return <button className={classNames(styles.base)}>Button</button>;
+export function Button({
+	backgroundColor = "#8ff8e2",
+	fontColor = "#313638",
+	fontBold = false,
+}: IButtonProps) {
+	return (
+		<button
+			style={{
+				backgroundColor: backgroundColor,
+				color: fontColor,
+				fontWeight: fontBold ? 400 : 600,
+			}}
+			className={classNames(styles.base)}
+		>
+			Button
+			<div
+				style={{
+					backgroundColor: lightenDarkenColor(backgroundColor, -40),
+				}}
+				className={classNames(styles.bottom)}
+			></div>
+		</button>
+	);
 }
