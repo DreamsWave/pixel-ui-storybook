ed620e7ef74c2c6bb875455af0ad283920cb7037
Alexey <dreamswave.dev@gmail.com>
2023-02-14 13:12:56 +0300

    fixed imports bug and added basic button

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,9 +1,26 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+	backgrounds: {
+		default: "default",
+		values: [
+			{
+				name: "default",
+				value: "#323353",
+			},
+			{
+				name: "light",
+				value: "#ffffff",
+			},
+			{
+				name: "dark",
+				value: "#313638",
+			},
+		],
+	},
+};
