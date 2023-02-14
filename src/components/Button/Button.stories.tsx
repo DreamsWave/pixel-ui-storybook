54caa9c140ae48313271bcac6d12db1d2e65f492
Alexey <dreamswave.dev@gmail.com>
2023-02-14 17:35:27 +0300

    made basic pixel button

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -5,10 +5,31 @@ import { Button } from "./Button";
 export default {
 	title: "Components/Button",
 	component: Button,
-	args: {},
+	args: {
+		backgroundColor: "#8ff8e2",
+		fontColor: "#313638",
+		fontBold: false,
+	},
+	argTypes: {
+		backgroundColor: {
+			type: {
+				name: "string",
+			},
+		},
+		fontColor: {
+			type: {
+				name: "string",
+			},
+		},
+	},
 } as Meta<typeof Button>;
 
 const Template: StoryFn<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {};
+
+export const FontBold = Template.bind({});
+Primary.args = {
+	fontBold: true,
+};
