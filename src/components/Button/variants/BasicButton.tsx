ce0e506e9157c71d61a679fd3072f62b5f60e814
Alexey <dreamswave.dev@gmail.com>
2023-02-27 13:04:59 +0300

    moving to layers based buttons

--- src/components/Button/variants/BasicButton.tsx
+++ src/components/Button/variants/BasicButton.tsx
@@ -20,99 +20,143 @@ function getBorderWidth(scale: number): number {
 	return scale * 3;
 }
 
-type Layer1Props = {
-	backgroundColor: string;
+const BaseButton = styled.button`
+	position: relative;
+	display: inline-flex;
+	background: transparent;
+	border: none;
+	padding: 0;
+	margin-bottom: 8px;
+	cursor: pointer;
+`;
+
+type ContentProps = {
 	fontColor: string;
 	fontBold: boolean;
 	fontSize: number;
-	borderWidth: number;
-	clipPath: number;
-	svg: string;
-	colorShades: string[];
+	backgroundColor: string;
+	children?: React.ReactNode;
 };
-const Layer1 = styled.span<Layer1Props>`
-	width: fit-content;
-	display: flex;
+const Content = styled.span<ContentProps>`
+	z-index: 10;
+	display: inline-flex;
 	justify-content: center;
 	align-items: center;
-	padding: 12px 30px;
 	font-size: ${(props) => props.fontSize}px;
 	font-family: "Press Start 2P", cursive;
 	font-weight: ${(props) => (props.fontBold ? 600 : 400)};
+	padding: 0.8em 2em;
 	text-transform: uppercase;
 	color: ${(props) =>
 		isContrastValid(props.fontColor, props.backgroundColor)
 			? props.fontColor
 			: getContrastColor(props.backgroundColor)};
-	background-color: transparent;
-	border-style: solid;
-	border-width: ${(props) => props.borderWidth}px;
-	border-color: #000;
-	border-image: url(${(props) => props.svg}) 3;
-	cursor: pointer;
-	transition: all 200ms ease-in-out;
 	white-space: nowrap;
-	position: relative;
-
-	&:hover&:before {
-		background-color: ${(props) => props.colorShades[4]};
-	}
-
-	&:active {
-		top: 4px;
-	}
-
-	&:active&:before {
-		background-color: ${(props) => props.backgroundColor};
-	}
-
-	&:before {
-		content: "";
-		position: absolute;
-		top: -${(props) => props.borderWidth - 1}px;
-		left: -${(props) => props.borderWidth - 1}px;
-		height: calc(100% + ${(props) => (props.borderWidth - 1) * 2}px);
-		width: calc(100% + ${(props) => (props.borderWidth - 1) * 2}px);
-		z-index: -1;
-		background-color: ${(props) => props.backgroundColor};
-		clip-path: polygon(
-			0 calc(0% + ${(props) => props.clipPath}px),
-			calc(0% + ${(props) => props.clipPath}px) 0,
-			calc(100% - ${(props) => props.clipPath}px) 0,
-			100% ${(props) => props.clipPath}px,
-			100% calc(100% - ${(props) => props.clipPath}px),
-			calc(100% - ${(props) => props.clipPath}px) 100%,
-			${(props) => props.clipPath}px 100%,
-			0% calc(100% - ${(props) => props.clipPath}px),
-			0% ${(props) => props.clipPath}px
-		);
-		transition: all 200ms;
-	}
+	transition: all 200ms;
 `;
 
-type Layer2Props = {
+type Layer1Props = {
 	backgroundColor: string;
-	fontColor: string;
-	fontBold: boolean;
-	fontSize: number;
 	borderWidth: number;
 	clipPath: number;
 	svg: string;
 	colorShades: string[];
 };
-const Layer2 = styled.span<Layer2Props>`
+const Layer1 = styled.div<Layer1Props>`
+	z-index: 9;
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	box-sizing: border-box;
+	background-color: transparent;
+	border-style: solid;
+	border-width: ${(props) => props.borderWidth}px;
+	border-color: #000;
+	border-image: url(${(props) => props.svg}) 3;
+	transition: all 200ms;
+`;
+
+type Layer2Props = {
+	backgroundColor: string;
+	borderWidth: number;
+	clipPath: number;
+	svg: string;
+	colorShades: string[];
+};
+const Layer2 = styled.div<Layer2Props>`
+	position: absolute;
+	top: 1px;
+	left: 1px;
+	height: calc(100% - 2px);
+	width: calc(100% - 2px);
+	z-index: 8;
+	background-color: ${(props) => props.backgroundColor};
+	clip-path: polygon(
+		0 calc(0% + ${(props) => props.clipPath}px),
+		calc(0% + ${(props) => props.clipPath}px) 0,
+		calc(100% - ${(props) => props.clipPath}px) 0,
+		100% ${(props) => props.clipPath}px,
+		100% calc(100% - ${(props) => props.clipPath}px),
+		calc(100% - ${(props) => props.clipPath}px) 100%,
+		${(props) => props.clipPath}px 100%,
+		0% calc(100% - ${(props) => props.clipPath}px),
+		0% ${(props) => props.clipPath}px
+	);
+	transition: all 200ms;
+`;
+
+type Layer3Props = {
+	backgroundColor: string;
+	borderWidth: number;
+	clipPath: number;
+	svg: string;
+	colorShades: string[];
+};
+const Layer3 = styled.div<Layer3Props>`
+	z-index: 7;
 	position: absolute;
 	bottom: -${(props) => props.borderWidth}px;
-	left: 0px;
-	height: calc(100% - ${(props) => props.borderWidth * 2}px);
-	width: calc(100% - ${(props) => props.borderWidth * 2}px);
-	background-color: ${(props) => props.colorShades[1]};
-	z-index: -2;
+	left: 0;
+	height: 100%;
+	width: 100%;
+	box-sizing: border-box;
 	border-style: solid;
 	border-width: ${(props) => props.borderWidth}px;
 	border-color: #000;
 	border-radius: ${(props) => props.borderWidth * 2.2}px;
 	border-image: url(${(props) => props.svg}) 3;
+	transition: all 200ms;
+`;
+
+type Layer4Props = {
+	backgroundColor: string;
+	borderWidth: number;
+	clipPath: number;
+	svg: string;
+	colorShades: string[];
+};
+const Layer4 = styled.div<Layer4Props>`
+	position: absolute;
+	bottom: -${(props) => props.borderWidth - 1}px;
+	left: 1px;
+	height: calc(100% - 2px);
+	width: calc(100% - 2px);
+	z-index: 6;
+	background-color: ${(props) => props.colorShades[1]};
+	clip-path: polygon(
+		0 calc(0% + ${(props) => props.clipPath}px),
+		calc(0% + ${(props) => props.clipPath}px) 0,
+		calc(100% - ${(props) => props.clipPath}px) 0,
+		100% ${(props) => props.clipPath}px,
+		100% calc(100% - ${(props) => props.clipPath}px),
+		calc(100% - ${(props) => props.clipPath}px) 100%,
+		${(props) => props.clipPath}px 100%,
+		0% calc(100% - ${(props) => props.clipPath}px),
+		0% ${(props) => props.clipPath}px
+	);
+	transition: all 200ms;
 `;
 
 export interface ButtonProps {
@@ -179,29 +223,50 @@ export function BasicButton({
 		setLayer2SVG(generateLayer2SVG(backgroundColor));
 	}, [backgroundColor]);
 	return (
-		<>
-			<Layer1
-				backgroundColor={backgroundColor}
+		<BaseButton
+			onMouseOver={() => {
+				console.log("mouse over");
+			}}
+			onMouseLeave={() => {
+				console.log("mouse leave");
+			}}
+		>
+			<Content
 				fontColor={fontColor}
 				fontBold={fontBold}
 				fontSize={fontSize}
+				backgroundColor={backgroundColor}
+			>
+				{children}
+			</Content>
+			<Layer1
+				backgroundColor={backgroundColor}
 				borderWidth={borderWidth}
 				clipPath={clipPath}
 				svg={layer1SVG}
 				colorShades={colorShades}
-			>
-				{children}
-			</Layer1>
+			/>
 			<Layer2
 				backgroundColor={backgroundColor}
-				fontColor={fontColor}
-				fontBold={fontBold}
-				fontSize={fontSize}
+				borderWidth={borderWidth}
+				clipPath={clipPath}
+				svg={layer1SVG}
+				colorShades={colorShades}
+			/>
+			<Layer3
+				backgroundColor={backgroundColor}
 				borderWidth={borderWidth}
 				clipPath={clipPath}
 				svg={layer2SVG}
 				colorShades={colorShades}
 			/>
-		</>
+			<Layer4
+				backgroundColor={backgroundColor}
+				borderWidth={borderWidth}
+				clipPath={clipPath}
+				svg={layer2SVG}
+				colorShades={colorShades}
+			/>
+		</BaseButton>
 	);
 }
