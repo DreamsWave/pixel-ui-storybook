676137a5e8cc93d8405a1959b86c20c9768dc059
Alexey <dreamswave.dev@gmail.com>
2023-03-02 22:01:18 +0300

    hide examples from storybook

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -4,7 +4,7 @@ import { StoryFn, Meta } from "@storybook/react";
 import { Header } from "./Header";
 
 export default {
-	title: "Example/Header",
+	// title: "Example/Header",
 	component: Header,
 	parameters: {
 		// More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
@@ -12,14 +12,14 @@ export default {
 	},
 } as Meta<typeof Header>;
 
-const Template: StoryFn<typeof Header> = (args) => <Header {...args} />;
+// const Template: StoryFn<typeof Header> = (args) => <Header {...args} />;
 
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-	user: {
-		name: "Jane Doe",
-	},
-};
+// export const LoggedIn = Template.bind({});
+// LoggedIn.args = {
+// 	user: {
+// 		name: "Jane Doe",
+// 	},
+// };
 
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {};
+// export const LoggedOut = Template.bind({});
+// LoggedOut.args = {};
--- src/stories/Page.stories.tsx
+++ src/stories/Page.stories.tsx
@@ -4,7 +4,7 @@ import { within, userEvent } from "@storybook/testing-library";
 import { Page } from "./Page";
 
 export default {
-	title: "Example/Page",
+	// title: "Example/Page",
 	component: Page,
 	parameters: {
 		// More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
@@ -14,13 +14,13 @@ export default {
 
 const Template: StoryFn<typeof Page> = (args) => <Page {...args} />;
 
-export const LoggedOut = Template.bind({});
+// export const LoggedOut = Template.bind({});
 
-export const LoggedIn = Template.bind({});
+// export const LoggedIn = Template.bind({});
 
-// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
-LoggedIn.play = async ({ canvasElement }) => {
-	const canvas = within(canvasElement);
-	const loginButton = await canvas.getByRole("button", { name: /Log in/i });
-	await userEvent.click(loginButton);
-};
+// // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
+// LoggedIn.play = async ({ canvasElement }) => {
+// 	const canvas = within(canvasElement);
+// 	const loginButton = await canvas.getByRole("button", { name: /Log in/i });
+// 	await userEvent.click(loginButton);
+// };
